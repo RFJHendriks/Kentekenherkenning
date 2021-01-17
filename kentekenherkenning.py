0a598759b29588e4eacae61b49ab12983a977c69
RFJHendriks <rickhendriks@gmail.com>
2021-01-17 12:42:50 +0100

    dataset uitgebreid, en karakter herkenning

--- kentekenherkenning.py
+++ kentekenherkenning.py
@@ -1,4 +1,6 @@
 # Loading the required python modules
+from random import randrange
+
 import pytesseract
 from pytesseract import Output
 import matplotlib.pyplot as plt
@@ -43,6 +45,8 @@ list_license_plates = []
 predicted_license_plates = []
 # pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 #
+counter = 10000
+counter_false = 0
 for path_to_license_plate in glob.glob(path_for_license_plates, recursive = True):
 
     license_plate_file = path_to_license_plate.split("/")[-1]
@@ -66,12 +70,12 @@ for path_to_license_plate in glob.glob(path_for_license_plates, recursive = True
     lower = np.array([16,100,100],dtype='uint8')
     upper = np.array([25,255,255],dtype='uint8')
     mask = cv2.inRange(hsv, lower, upper)
-    cv2.imshow("color", img)
-    cv2.imshow("mask", mask)
+    # cv2.imshow("color", img)
+    # cv2.imshow("mask", mask)
     mask = cv2.GaussianBlur(mask, (5,5),5)
 
     edged = cv2.Canny(mask, 30, 200)
-    cv2.imshow("edges",edged)
+    # cv2.imshow("edges",edged)
 
     contours = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = imutils.grab_contours(contours)
@@ -86,7 +90,7 @@ for path_to_license_plate in glob.glob(path_for_license_plates, recursive = True
         peri = cv2.arcLength(c, True)
         approx = cv2.approxPolyDP(c, 0.018*peri, True)
         # print(license_plate + ' contours length: ' + str(len(approx)))
-        if len(approx) == 4 or len(approx) == 5:
+        if len(approx) == 4:
             masked = cv2.drawContours(masked, c, -1, (0,255,0),3)
             screenCnt = approx
             break
@@ -184,14 +188,80 @@ for path_to_license_plate in glob.glob(path_for_license_plates, recursive = True
 
     if maxWidth < maxHeight:
         out = cv2.rotate(out, cv2.ROTATE_90_CLOCKWISE)
-    if len(screenCnt) == 5:
-        out = cv2.rotate(out, cv2.ROTATE_180)
+    # if len(screenCnt) == 5:
+    #     out = cv2.rotate(out, cv2.ROTATE_180)
 
     path = 'F:/OpenCV/Nummerplaat/'
     imagename = license_plate + '.jpg'
     cv2.imwrite(os.path.join(path + imagename), out)
 
-    testAfbeelding = license_plate
+    (h, w) = out.shape[:2]
+    width = 500
+    r = width / float(w)
+    dim = (width, int(h*r))
+    testAfbeelding = cv2.resize(out, dim)
+
+    testAfbeelding = cv2.cvtColor(testAfbeelding,cv2.COLOR_BGR2GRAY)
+    testAfbeelding = cv2.GaussianBlur(testAfbeelding, (7,7), 0)
+    _,testOTSU = cv2.threshold(testAfbeelding,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    def sort_contours(cnts, reverse = False):
+        i = 0
+        boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+        (cnts, boundingBoxes) = zip(*sorted(zip(cnts,boundingBoxes), key=lambda b: b[1][i], reverse=reverse))
+        return cnts
+
+    #gewenste breedte, hoogte voor karakters
+    digit_w, digit_h = 30, 60
+    crop_characters = []
+
+    contours, _ = cv2.findContours(testOTSU, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for c in sort_contours(contours):
+        (x,y,w,h) = cv2.boundingRect(c)
+        ratio = h/w
+        if 1<=ratio<=5.5: #controleren of vorm overeenkomt met wat verwacht mag worden van karakter
+            if h/testOTSU.shape[0]>0.5: #controleren of het groot genoeg is om een karakter te zijn
+                curr_num = testOTSU[y:y+h,x:x+w] #karakter uitsnijden
+                curr_num = cv2.resize(curr_num, dsize=(digit_w, digit_h))
+                _, curr_num = cv2.threshold(curr_num, 220,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+                crop_characters.append(curr_num)
+    if len(crop_characters) == 6:
+        for i in range(len(crop_characters)):
+            if i<=5:
+                titel = license_plate[i]
+                path = 'F:/OpenCV/Nummerplaat/Karakters/'
+                img_name = license_plate[i] + "_" + str(counter) + ".jpg"
+                testtext = path + img_name
+                cv2.imwrite(os.path.join(path + img_name), crop_characters[i])
+                counter += 1
+                text_out = pytesseract.image_to_string(crop_characters[i], config='-c tessedit_char_whitelist=ABDEFGHJKLMNOPRSTUVWXZ0123456789 --psm 11')
+                print("letter: " + titel + " voorspelling: " + text_out)
+                cv2.imshow("test", crop_characters[i])
+                cv2.waitKey(0)
+    else:
+        print(license_plate)
+        counter_false += 1
+
+
+
+
+
+
+    # for c in sort_contours(contours):
+    #     print(c)
+    #     (x,y,w,h) = cv2.boundingRect(c)
+    #     ratio = w/h
+    #     print(w, " height: ", h)
+    #     if 1<=ratio<=3.5: #alleen als ratio klopt met wat we verwachten van een karakter
+    #         print("hoi")
+    #         if h/testOTSU.shape[0]>0.5: #alleen als karakter minstens 50% van de hoogte van het kenteken heeft
+    #             cv2.rectangle(test_roi, (x,y), (x+w, y+h), (0,255,0), 2)
+    #             curr_num = testOTSU[y:y+h,x:x+w]
+    #             curr_num = cv2.resize(curr_num, dsize=(digit_w, digit_h))
+    #             _, curr_num = cv2.threshold(curr_num, 220, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #             crop_characters.append(curr_num)
+    #
+    # print("Detect {} letters...".format(len(crop_characters)))
 
     mask = np.zeros(gray.shape,np.uint8)
     new_image = cv2.drawContours(mask,[screenCnt],0,255,-1)
@@ -221,15 +291,22 @@ for path_to_license_plate in glob.glob(path_for_license_plates, recursive = True
     edgesGray = cv2.Canny(CroppedBlur.copy(),100,200)
     _,th2 = cv2.threshold(CroppedBlur, 0, 255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     # CroppedBlur = cv2.adaptiveThreshold(CroppedBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
-    text = pytesseract.image_to_string(out, config='-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789- --psm 13')
-    textBlur = pytesseract.image_to_string(out, config='-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --psm 11')
-    textOtsu = pytesseract.image_to_string(edgescolor, config='-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 --psm 11')
+    text =          pytesseract.image_to_string(out, config='-c tessedit_char_whitelist=ABDEFGHJKLMNOPRSTUVWXZ0123456789 --psm 13')
+    textBlur =      pytesseract.image_to_string(out, config='-c tessedit_char_whitelist=ABDEFGHJKLMNOPRSTUVWXZ0123456789 --psm 11')
+    textOtsu = pytesseract.image_to_string(testOTSU, config='-c tessedit_char_whitelist=ABDEFGHJKLMNOPRSTUVWXZ0123456789 --psm 11')
     text = "".join(text.split()).replace(":","").replace("-", "")
     textBlur = "".join(textBlur.split()).replace(":","").replace("-", "")
-    print('werkelijke license plate: ' + license_plate + ' gelezen kleur: ' + text + ' blurred: ' +textBlur + ' otsu: ' + textOtsu)
-    cv2.imshow("blaat", CroppedBlur)
-    cv2.imshow("blaat2", edgesGray)
-    cv2.waitKey(0)
+
+    # aantalKarakters = format(len(crop_characters))
+    # if len(crop_characters)!=6:
+    #     print(license_plate)
+    #     cv2.imshow('test', testOTSU)
+    #     cv2.waitKey(0)
+
+    # print('werkelijke license plate: ' + license_plate + ' gelezen kleur: ' + text + ' blurred: ' +textBlur + ' otsu: ' + textOtsu + 'karakters: ' + aantalKarakters)
+    # cv2.imshow("blaat", CroppedBlur)
+    # cv2.imshow("blaat2", edgesGray)
+    # cv2.waitKey(0)
     # transform_mat = cv2.getPerspectiveTransform(Cropped, [[0,200], [0,0],[0,800],[200,800]])
 
 
@@ -281,3 +358,6 @@ for path_to_license_plate in glob.glob(path_for_license_plates, recursive = True
 #
 #
 # calculate_predicted_accuracy(list_license_plates, predicted_license_plates)
+aantalnummerplaten = len(list_license_plates)
+
+print("aantal: " + str(aantalnummerplaten) + " foutief: " + str(counter_false))
--- nummerplate.py
+++ nummerplate.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from local_utils import detect_lp
-from os.path import splitext,basename
+from os.path import splitext, basename
 from keras.models import model_from_json
 import glob
+
 
 def load_model(path):
     try:
@@ -23,92 +24,97 @@ wpod_net_path = "wpod-net.json"
 wpod_net = load_model(wpod_net_path)
 
 
-def preprocess_image(image_path,resize=False):
+def preprocess_image(image_path, resize=False):
     img = cv2.imread(image_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img / 255
     if resize:
-        img = cv2.resize(img, (224,224))
+        img = cv2.resize(img, (224, 224))
     return img
+
 
 # Create a list of image paths
 image_paths = glob.glob("nummerplaat/*.jpg")
-print("Found %i images..."%(len(image_paths)))
+print("Found %i images..." % (len(image_paths)))
 
 # Visualize data in subplot
-fig = plt.figure(figsize=(12,8))
+fig = plt.figure(figsize=(12, 8))
 cols = 5
 rows = 4
 fig_list = []
-for i in range(cols*rows):
-    fig_list.append(fig.add_subplot(rows,cols,i+1))
+for i in range(cols * rows):
+    fig_list.append(fig.add_subplot(rows, cols, i + 1))
     title = splitext(basename(image_paths[i]))[0]
     fig_list[-1].set_title(title)
-    img = preprocess_image(image_paths[i],True)
+    img = preprocess_image(image_paths[i], True)
     plt.axis(False)
     plt.imshow(img)
 
 plt.tight_layout(True)
 plt.show()
 
+
 # forward image through model and return plate's image and coordinates
 # if error "No Licensese plate is founded!" pop up, try to adjust Dmin
-def get_plate(image_path, Dmax=640, Dmin=100):
+def get_plate(image_path, d_max=640, d_min=100):
     vehicle = preprocess_image(image_path)
     ratio = float(max(vehicle.shape[:2])) / min(vehicle.shape[:2])
-    side = int(ratio * Dmin)
-    bound_dim = min(side, Dmax)
-    _ , LpImg, _, cor = detect_lp(wpod_net, vehicle, bound_dim, lp_threshold=0.5)
+    side = int(ratio * d_min)
+    bound_dim = min(side, d_max)
+    _, LpImg, _, cor = detect_lp(wpod_net, vehicle, bound_dim, lp_threshold=0.5)
     return LpImg, cor
+
 
 # Obtain plate image and its coordinates from an image
 test_image = image_paths[2]
-LpImg,cor = get_plate(test_image)
-print("Detect %i plate(s) in"%len(LpImg),splitext(basename(test_image))[0])
+LpImg, cor = get_plate(test_image)
+print("Detect %i plate(s) in" % len(LpImg), splitext(basename(test_image))[0])
 print("Coordinate of plate(s) in image: \n", cor)
 
 # Visualize our result
-plt.figure(figsize=(12,5))
-plt.subplot(1,2,1)
+plt.figure(figsize=(12, 5))
+plt.subplot(1, 2, 1)
 plt.axis(False)
 plt.imshow(preprocess_image(test_image))
-plt.subplot(1,2,2)
+plt.subplot(1, 2, 2)
 plt.axis(False)
 plt.imshow(LpImg[0])
 
-#plt.savefig("part1_result.jpg",dpi=300)
+
+# plt.savefig("part1_result.jpg",dpi=300)
 
 def draw_box(image_path, cor, thickness=3):
-    pts=[]
-    x_coordinates=cor[0][0]
-    y_coordinates=cor[0][1]
+    pts = []
+    x_coordinates = cor[0][0]
+    y_coordinates = cor[0][1]
     # store the top-left, top-right, bottom-left, bottom-right
     # of the plate license respectively
     for i in range(4):
-        pts.append([int(x_coordinates[i]),int(y_coordinates[i])])
+        pts.append([int(x_coordinates[i]), int(y_coordinates[i])])
 
     pts = np.array(pts, np.int32)
-    pts = pts.reshape((-1,1,2))
+    pts = pts.reshape((-1, 1, 2))
     vehicle_image = preprocess_image(image_path)
 
-    cv2.polylines(vehicle_image,[pts],True,(0,255,0),thickness)
+    cv2.polylines(vehicle_image, [pts], True, (0, 255, 0), thickness)
     return vehicle_image
 
-plt.figure(figsize=(8,8))
+
+plt.figure(figsize=(8, 8))
 plt.axis(False)
-plt.imshow(draw_box(test_image,cor))
+plt.imshow(draw_box(test_image, cor))
 
 # Viualize all obtained plate images
-fig = plt.figure(figsize=(12,6))
+fig = plt.figure(figsize=(12, 6))
 cols = 5
 rows = 4
 fig_list = []
 
-for i in range(cols*rows):
-    fig_list.append(fig.add_subplot(rows,cols,i+1))
+for i in range(cols * rows):
+    fig_list.append(fig.add_subplot(rows, cols, i + 1))
     title = splitext(basename(image_paths[i]))[0]
     fig_list[-1].set_title(title)
-    LpImg,_ = get_plate(image_paths[i])
+    LpImg, _ = get_plate(image_paths[i])
     plt.axis(False)
     plt.imshow(LpImg[0])
 
